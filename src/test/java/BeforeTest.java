8c866aaf747b4a4ba840ed28cb6f5678831c57c5
Padavan1 <dpastushenko1996@mail.com>
2016-11-15 12:37:46 +0200

    This is a new commit for what I originally planned to be amended

--- src/test/java/BeforeTest.java
+++ src/test/java/BeforeTest.java
@@ -15,7 +15,7 @@ public class BeforeTest {
     @BeforeClass
     public void setUp() throws MalformedURLException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Denys\\Desktop\\chromedriver_win32\\chromedriver.exe");
-        homePage = new HomePage(new RemoteWebDriver(new URL("http://localhost:4445/wd/hub"), DesiredCapabilities.chrome()));
+        homePage = new HomePage(new RemoteWebDriver(new URL("http://localhost:4445/wd/hub"), DesiredCapabilities.firefox()));
         // photoPage = new PageObject(new ChromeDriver());
         Page.getDriver().manage().window().maximize();
     }
