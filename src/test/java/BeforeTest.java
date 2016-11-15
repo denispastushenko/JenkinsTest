e31a0a64d5ef7749aedbda1c8d1401022dd2cb50
Padavan1 <dpastushenko1996@mail.com>
2016-11-15 13:56:16 +0200

    Comm

--- src/test/java/BeforeTest.java
+++ src/test/java/BeforeTest.java
@@ -7,6 +7,7 @@ import pages.HomePage;
 import pages.Page;
 import pages.TestPage;
 
+import java.awt.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -17,14 +18,15 @@ public class BeforeTest {
     protected AssertPage assertPage;
 
     @Before
-
-    public void setUp() throws MalformedURLException {
+    public void setUp() throws MalformedURLException, AWTException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Denys\\Desktop\\chromedriver_win32\\chromedriver.exe");
-        homePage = new HomePage(new RemoteWebDriver(new URL("http://localhost:4445/wd/hub"), DesiredCapabilities.firefox()));
+        homePage = new HomePage(new RemoteWebDriver(new URL("http://localhost:4445/wd/hub"), DesiredCapabilities.chrome()));
         Page.getDriver().manage().window().maximize();
     }
 
+
     @After
+
     public void tearDown() throws Exception {
         if (Page.getDriver() != null) {
             Page.getDriver().quit();
--- src/test/java/JenkinsTest.java
+++ src/test/java/JenkinsTest.java
@@ -1,17 +1,16 @@
 import org.junit.Test;
 import pages.Page;
 
-public class MainTest extends  BeforeTest {
-
+public class JenkinsTest extends BeforeTest{
     @Test
-    public void testMethod() throws Exception {
+    public void signInTest() throws Exception {
         Page.getDriver().get(site);
         testPage = homePage.testURL("epam systems");
         assertPage = testPage.getLinkIdeas();
         assertPage = assertPage.assertTest("Идеи и проекты");
     }
     @Test
-    public void testMethodError() throws Exception {
+    public void signInTestErr() throws Exception {
         Page.getDriver().get(site);
         testPage = homePage.testURL("epam systems");
         assertPage = testPage.getLinkIdeas();
